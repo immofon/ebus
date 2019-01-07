fc0e490c906f2c8090ca1dd1223762d3c776a08a
mofon <yzkf000@foxmail.com>
2019-01-07 14:34:32 +0800

    update

--- bus/clip.go
+++ bus/clip.go
@@ -13,8 +13,8 @@ func clip() {
 		recover()
 	}()
 
-	//c := NewClient("ws://39.105.42.45:8100/", func(e ebus.Event) {
-	c := NewClient("ws://localhost:8100/", func(e ebus.Event) {
+	c := NewClient("ws://39.105.42.45:8100/", func(e ebus.Event) {
+		//c := NewClient("ws://localhost:8100/", func(e ebus.Event) {
 	})
 
 	ch := make(chan string)
